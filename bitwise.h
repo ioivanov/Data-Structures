264d6c37491638620bc6038e1a29df99133ad5fb
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-10-05 09:59:41 +0300

    Задача за битови масиви.

---
+++ bitwise.h
@@ -0,0 +1,30 @@
+#ifndef BITWISE_H_
+#define BITWISE_H_
+
+#include <iostream>
+using namespace std;
+
+typedef unsigned long bits;
+const int BITS_IN_BYTE = 8;
+
+class Bitwise {
+	bits b;
+
+public:
+	Bitwise(int _b = 0) : b(_b) {}
+
+	int length() const { return sizeof(bits) * BITS_IN_BYTE; }
+
+	// Мутатори
+	void set(int i);
+	void reset(int i);
+
+	// Селектор
+	bool get(int i) const;
+
+
+};
+
+ostream& operator<<(ostream&, Bitwise const&);
+
+#endif /* BITWISE_H_ */
