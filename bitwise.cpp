98d4fe66e5dca6c71fb2e78485637ac928891743
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-10-05 10:51:51 +0300

    Create bitwise.cpp

---
+++ bitwise.cpp
@@ -0,0 +1,22 @@
+#include "bitwise.h"
+
+void Bitwise::set(int i) {
+	bits mask = 1L << i;
+	b |= mask;
+}
+
+void Bitwise::reset(int i) {
+	bits mask = ~(1L << i);
+	b &= mask;
+}
+
+bool Bitwise::get(int i) const {
+	bits mask = 1L << i;
+	return b & mask;
+}
+
+ostream& operator <<(ostream& os, const Bitwise& bw) {
+	for(int i = 0; i < bw.length(); i++)
+		os << bw.get(i);
+	return os;
+}
