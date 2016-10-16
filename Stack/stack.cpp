ffc7b8c346f92d156e393c6bb0ac0d84d5c54859
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-10-16 14:42:44 +0300

    Create stack.cpp

---
+++ Stack/stack.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#include "stack.h"
+
+int const EMPTY_STACK = -1; // отбелязваме празен стек
+
+Stack::Stack() {
+	top = EMPTY_STACK;
+}
+
+bool Stack::empty() const {
+	return top == EMPTY_STACK;
+}
+
+int Stack::top() const {
+	if (empty()) {
+		cout << "Грешка: опит за поглеждане в празен стек!\n";
+		return 0;
+	}
+
+	return a[top];
+}
+
+void Stack::push(int const& x) {
+	if (full()) {
+		cout << "Грешка: опит за включване в пълен стек!\n";
+	} else
+		a[++top] = x;
+}
+
+bool Stack::full() const {
+	return top == MAX - 1;
+}
+
+int Stack::pop() {
+	if (empty()) {
+		cout << "Грешка: опит за изключване от празен стек!\n";
+		return 0;
+	}
+	return a[top--];
+}
