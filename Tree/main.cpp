9a51ea175e6b56369e9b75c6f2ec36bcb6a3b31e
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-11-22 14:38:11 +0200

    Create main.cpp

---
+++ Tree/main.cpp
@@ -0,0 +1,9 @@
+	t1.addChild(TestTree(2)
+				.addChild(TestTree(3))
+				.addChild(TestTree(4)
+						  .addChild(TestTree(5))
+						  .addChild(TestTree(6)))
+			    .addChild(TestTree(7))
+			    .addChild(TestTree(8)))
+	  .addChild(TestTree(9))
+.addChild(TestTree(10));
