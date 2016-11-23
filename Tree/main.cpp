f2b81e52e87c51fc7e4edbf834b7767ced4ca159
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-11-23 16:23:56 +0200

    Update main.cpp

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,10 +1,8 @@
 Tree<int> t1(1);
-t1.addChild(TestTree(2)
-				.addChild(TestTree(3))
-				.addChild(TestTree(4)
-						  .addChild(TestTree(5))
-						  .addChild(TestTree(6)))
-			    .addChild(TestTree(7))
-			    .addChild(TestTree(8)))
-	  .addChild(TestTree(9))
-.addChild(TestTree(10));
+t1.addChild(TestTree(2).addChild(TestTree(3))
+		       .addChild(TestTree(4).addChild(TestTree(5))
+				            .addChild(TestTree(6)))
+		       .addChild(TestTree(7))
+		       .addChild(TestTree(8)))
+  .addChild(TestTree(9))
+  .addChild(TestTree(10));
