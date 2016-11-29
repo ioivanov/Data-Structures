9eb007a3c450e56043ee60abba2a6ca82fda8e0a
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2016-11-29 11:21:54 +0200

    Update tree.cpp

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -1,39 +1,44 @@
+#include "stdafx.h"
+#include <list>
 #include <iostream>
-using namespace std;
-#include "linked_list.cpp"
+using std::list;
+using std::ostream;
 
-template <typename T>
 class Tree {
 private:
-	T data;
-	LinkedList<Tree<T>> children;
-	// !!! Tree t;
-	// Tree* t;
+	int root;
+	// списък от децата (наследниците на корена)
+	list<Tree> subtrees;
 
 public:
+	// при конструирането на дърво задаваме само корена
+	// за добавяне на деца използваме addChild
+	Tree(int _root = 0) : root(_root) {}
 
-	using I = LinkedListIterator<Tree<T>>;
-
-	Tree(T const& _data = T()) : data(_data) {}
-
-	Tree<T>& addChild(Tree<T> const& c) {
-		children.insertEnd(c);
+	// добавя поддърво child към списъка с поддърветата subtrees
+	// връща референция към дървото,
+	// за да можем да викаме addChild няколко пъти за дървотo, т.е така:
+	// tree.addChild(1).addChild(2).addChild(3);
+	Tree& addChild(Tree const& child)
+	{
+		subtrees.push_back(child);
 		return *this;
 	}
 
-	T root() const {
-		return data;
-	}
+	// връща стойността на корена
+	int getRoot() const { return root; }
 
-	I subtrees() const {
-		return children.begin();
+	// връща референция към списъка от поддървета
+	list<Tree> const& getSubtrees() const { return subtrees; }
+
+	// извежда дървото
+	friend ostream& operator<<(ostream& os, Tree const& tree)
+	{
+		os <<'('<< tree.getRoot();
+		for (list<Tree>::const_iterator it = tree.getSubtrees().begin(); it != tree.getSubtrees().end(); it++)
+		{
+			os <<' '<< *it;
+		}
+		return os << ')';
 	}
 };
-
-template <typename T>
-ostream& operator<<(ostream& os, Tree<T> const& t) {
-	os << '(';
-	for(typename Tree<T>::I it = t.subtrees();it;++it)
-		os << *it << ' ';
-	return os << t.root() << ')';
-}
