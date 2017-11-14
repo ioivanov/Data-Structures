5511642a7612b730f02c3ed1790c44c8655c5457
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2017-11-14 23:14:42 +0200

    Update linked_list.cpp

--- List/linked_list.cpp
+++ List/linked_list.cpp
@@ -1,211 +1,200 @@
+#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
-#include "list.h"
-
 template <typename T>
-struct LinkedListElement {
-	T data;
-	LinkedListElement* next;
+class List
+{
+	struct Element
+	{
+		T data;
+		Element* next;
 
-	LinkedListElement(T _data = T(), LinkedListElement* _next = NULL)
-		: data(_data), next(_next) {}
-};
+		Element(T _data = T(), Element* _next = nullptr)
+			: data(_data), next(_next) {}
+	};
 
-template <typename T>
-class LinkedList;
+	Element *front;
+	//може да има и указател към края на списъка с цел добавяне
+	//на елементи в края за константно време
 
-template <typename T>
-class LinkedListIterator : public Iterator<T, LinkedListIterator<T> > {
-	LinkedListElement<T>* p;
-public:
+	void copy(List const& l)
+	{
+		if (l.front == nullptr)
+			return;
 
-	LinkedListIterator(LinkedListElement<T>* _p = NULL) : p(_p) {}
+		Element* toCopy = l.front; //с този указател обхождаме 
+		//елементите на списъка от който копираме
+		Element* newElement = new Element(toCopy->data); //този указател
+		//ще помни адреса на новия елемент
+		Element* lastCopied = newElement; //сочи към последния елемент
+		//в новия списък
+		front = newElement;
 
-	typedef Iterator<T, LinkedListIterator<T> > I;
-
-	// оправя проблемите с използването на наследените префиксни оператори
-	using I::operator++;
-	using I::operator--;
-
-	// придвижва итератора напред и връща новата позиция
-	// O(1)
-	I& operator++() {
-		if (p != NULL)
-			p = p->next;
-		return *this;
+		while(toCopy->next!=nullptr)
+		{
+			toCopy = toCopy->next; //обхождаме елементите на
+			//списъка подаден като аргумент
+			newElement = new Element(toCopy->data); //заделяме 
+			//нова памет
+			lastCopied->next = newElement; //поставяме новия
+			//елемент в края на новия списък
+			lastCopied = newElement; //последния елемент
+			//на новия списък вече е новия елемент
+		}
 	}
 
-
-	// придвижва итератора назад и връща новата позиция
-	I& operator--() {
-		// не можем да се връщаме назад!!!
-		return *this;
+	void clean()
+	{
+		Element* toDelete = front;
+		Element* tmp;
+		//използваме тези два указателя за да изтрием елементите
+		//на списъка един по един без да губим верижката
+		while(toDelete!=nullptr)
+		{
+			tmp = toDelete;
+			toDelete = toDelete->next;
+			delete tmp;
+		}
 	}
 
-	// достъп до елемент
-	// O(1)
-	T& operator*() const {
-		// ВНИМАНИЕ: итераторът трябва да е валиден!
-		return p->data;
-	}
+	Element* findElement(int index) const
+	{
+		//трябва да върнем указател към кутийката с подадения индекс
+		Element* currentElement = front;
+		//опитваме се да стигнем до този индекс
+		for (int i = 0; i < index; i++)
+		{
+			//ако достигнем края на списъка преди достигането на идекса
+			if (currentElement == nullptr)
+				return nullptr;
 
-	// проверка за валидност
-	// O(1)
-	operator bool() const {
-		return p != NULL;
-	}
-
-	bool operator==(I const& it) const {
-		return p == ((LinkedListIterator<T> const&)it).p;
-	}
-
-	friend class LinkedList<T>;
-
-};
-
-template <typename T>
-class LinkedList : public List<T, LinkedListIterator<T> > {
-public:
-
-	typedef LinkedListIterator<T> I;
-	typedef LinkedListElement<T> E;
-
-private:
-	LinkedListElement<T> *front, *back;
-
-	// O(n)
-	I findPrev(I it) {
-		I pit = begin();
-		while (pit && pit.p->next != it.p)
-			++pit;
-		// хващаме случаите:
-		// 1. списъкът е празен
-		// 2. търсим преди първия
-		// 3. итераторът е невалиден
-		return pit;
+			currentElement = currentElement->next;
+		}
+		return currentElement;
 	}
 
 public:
-	LinkedList() : front(NULL), back(NULL) {}
+	List() : front(nullptr){}
 
-	LinkedList(LinkedList const& l) : front(NULL), back(NULL) {
+	List(List const& l) : front(nullptr) 
+	{
 		this->copy(l);
 	}
 
-	LinkedList& operator=(LinkedList const& l) {
-		if (this != &l) {
+	List& operator=(List const& l) 
+	{
+		if (this != &l) 
+		{
 			this->clean();
 			this->copy(l);
 		}
 		return l;
 	}
 
-	~LinkedList() {
+	~List() {
 		this->clean();
 	}
 
-	// проверка за празнота
-	// O(1)
-	bool empty() const {
-		return front == NULL;
+	bool empty() const 
+	{
+		return front == nullptr;
 	}
 
 	// включване преди позиция
-	// O(n)
-	bool insertBefore(T const& x, I const& it) {
-		// вмъква нов първи елемент, дори и списъкът да е празен!
-		if (front == it.p || empty()) {
-			front = new E(x, front);
-			if (back == NULL)
-				back = front;
+	bool insertBefore(T const& x, int index) 
+	{
+		if (index < 0)
+			return false;
+		if(index==0 || empty()) //ако списъка е празен или
+		//вмъкваме преди първия елемент
+		{
+			front = new Element(x, front); //създаваме нов елемент за
+			//начало на списъка, като го насочваме към предишното начало
 			return true;
 		}
-		I pit = findPrev(it);
-		return insertAfter(x, pit);
+		//в противен случай може да извикаме следната функция
+		return insertAfter(x, index - 1);
 	}
 
 	// включване след позиция
-	// O(1)
-	bool insertAfter(T const& x, I const& it) {
-		if (empty()) {
-			front = back = new E(x);
+	bool insertAfter(T const& x, int index) 
+	{
+		//ако списъка е празен слагаме новия елемент в началото
+		if (empty()) 
+		{
+			front = new Element(x);
 			return true;
 		}
-		// front != NULL =! back
-		if (!it)
+
+		Element* currentElement = findElement(index);
+		//ако не намерим такъв индекс
+		if (currentElement==nullptr)
 			return false;
-		it.p->next = new E(x, it.p->next);
-		if (back == it.p)
-			back = back->next;
+		
+		//аналогично на ред 115
+		currentElement->next = new Element(x, currentElement->next);
 		return true;
 	}
 
+	/*
+	 * При фунцкиите за изтриване на елемент може да се направи така, че
+	 * да връщат стойността на елемента, който изтриват.
+	 * Точно това е ролята на първия аргумент при следващите 3 функции.
+	 */
+
 	// изключване преди позиция
-	// O(n)
-	bool deleteBefore(T& x, I const& it) {
-		I pit = findPrev(it);
-		return deleteAt(x, pit);
+	bool deleteBefore(T& x, int index) 
+	{
+		//ако списъка не е празен и не се опитваме да изтрием
+		//елемента преди първи то спокойно можем да извикаме
+		//функцията за изтриване на дадена позиция
+		if (index == 0 || empty())
+			return false;
+		return deleteAt(x, index - 1);
 	}
 
 	// изключване на позиция
-	// O(n)
-	bool deleteAt(T& x, I const& it) {
-		if (!empty() && front == it.p) {
+	bool deleteAt(T& x, int index) 
+	{
+		if (empty())
+			return false;
+		//ако не изтриваме първия елемент то изтиването
+		//може да се изпълни от следващата фунцкия
+		if (index == 0)
+		{
 			x = front->data;
-			E* toDelete = front;
+			Element* toDelete = front;
 			front = front->next;
-			if (back == toDelete)
-				back = NULL;
 			delete toDelete;
+			return true;
 		}
-		// front != it.p
-		// не се опитваме да изтрием първия елемент
-		I pit = findPrev(it);
-		return deleteAfter(x, pit);
+
+		return deleteAfter(x, index - 1);
 	}
 
 	// изключване след позиция
-	// O(1)
-	bool deleteAfter(T& x, I const& it) {
-		if (!it || it.p->next == NULL)
+	bool deleteAfter(T& x, int index) 
+	{
+		//намираме елемента, който ще трием
+		Element* currentElement = findElement(index);
+		if (currentElement == nullptr || currentElement->next == nullptr)
 			return false;
-		// it.p != NULL && it.p->next != NULL
-		E* toDelete = it.p->next;
-		it.p->next = toDelete->next;
+		
+		//ако е легитимен елемент го изтриваме
+		Element* toDelete = currentElement->next;
+		currentElement->next = toDelete->next;
 		x = toDelete->data;
-		if (back == toDelete)
 
-			back = it.p;
 		delete toDelete;
 		return true;
 	}
 
 	// достъп до елемент на позиция
-	// O(1)
-	T& elementAt(I const& it) {
-		return *it;
+	// !трябва да се добави сигурност!
+	T const& elementAt(int index) const
+	{
+		Element* element = findElement(index);
+		return element->data;
 	}
-
-	// връща позицията "начало на списъка"
-	I begin() const {
-		return I(front);
-	}
-
-	// връща позицията "край на списъка"
-	I end() const {
-		return I(back);
-	}
-
-	// O(1)
-	void append(LinkedList<T>& l2) {
-		if (empty())
-			front = l2.front;
-		else
-			back->next = l2.front;
-		back = l2.back;
-		l2.front = l2.back = NULL;
-	}
-
-
 };
